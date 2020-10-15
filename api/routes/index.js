c0b1e856c65967ad71465742049f1cd486142d3c
darwinyee <darwinmtyee@gmail.com>
2020-10-15 12:47:52 -0700

    test push

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.get("/", function(req, res, next) {
     res.send("API is working properly");
     //res.render('index', { title: 'Express' });
+    //test push
 });
 
 module.exports = router;
