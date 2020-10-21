f1a872e0fafc310ef55718a88a3e40c6bc919456
sgilbo618@gmail.com <sgilbo618@gmail.com>
2020-10-21 19:15:59 -0400

    Add browse by category section

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -56,23 +56,55 @@ const Home = () => {
         </Row>
         <Row>
             <Col md={{span: 2, offset: 2}}>
-              <img className="info-img" src={require("../images/disinfect_200.png")} />
+              <img className="info-img" alt="hand washing icon" src={require("../images/disinfect_200.png")} />
               <p className="info-text">Thoroughly disinfected bicycles for your protection and convenience</p>
             </Col>
             <Col md={{span: 2, offset: 1}}>
-              <img className="info-img" src={require("../images/contact_200.png")} />
+              <img className="info-img" alt="person talking icon" src={require("../images/contact_200.png")} />
               <p className="info-text">No contact pick up - Pick up your bike with peace of mind</p>
             </Col>
             <Col md={{span: 2, offset: 1}}>
-              <img className="info-img" src={require("../images/cancel_bike_200.png")} />
+              <img className="info-img" alt="no bike sign" src={require("../images/cancel_bike_200.png")} />
               <p className="info-text">Need to cancel? You can cancel your reservation for free up to 24 hours prior</p>
             </Col>
         </Row>
+        <hr/>
 
         {/* Browse by category */}
         <Row>
-
+          <Col>
+            <h1 className="browse-title">Browse by category</h1>
+          </Col>
         </Row>
+        <Row>
+          <Col>
+            <img className="browse-img" alt="road bike" src={require("../images/road_bike.jpg")} />
+            <p className="browse-text">Road Bikes</p>
+          </Col>
+          <Col>
+            <img className="browse-img" alt="electric bike" src={require("../images/electric_bike.jpg")} />
+            <p className="browse-text">Electric Bikes</p>
+          </Col>
+          <Col>
+            <img className="browse-img" alt="mountain bike" src={require("../images/mountain_bike.jpg")} />
+            <p className="browse-text">Mountain Bikes</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <img className="browse-img" alt="hybrid bike" src={require("../images/hybrid_bike.jpg")} />
+            <p className="browse-text">Hybrid Bikes</p>
+          </Col>
+          <Col>
+            <img className="browse-img" alt="comfort bike" src={require("../images/comfort_bike.jpg")} />
+            <p className="browse-text">Comfort Bikes</p>
+          </Col>
+          <Col>
+            <img className="browse-img" alt="kids bike" src={require("../images/kids_bike.jpg")} />
+            <p className="browse-text">Kids Bikes</p>
+          </Col>
+        </Row>
+
       </Container-fluid>
     </div>
   )
