fe9124c0bfa3ee06a895e3283c4f55d5f34dadf0
darwinyee <darwinmtyee@gmail.com>
2020-10-21 10:12:40 -0700

    Add error msg on bikeTable page for debugging

--- api/routes/bikeTable.js
+++ api/routes/bikeTable.js
@@ -12,9 +12,11 @@ router.get('/', (req, res) => {
     let query = 'SELECT b.bike_id,b.price,b.bike_details,u.user_name,u.email,l.address,l.city,l.state,l.zip' +
                 ' FROM bike b inner join user u on b.user_id = u.user_id ' + 
                 'inner join location l on b.location_id = l.location_id;'
+
     pool.query(query, (err, result)=>{
         if(err){
-            res.send({err:err});
+            res.send({data:[],err:err,hasError:1});
+            
         }else{
             let items = [];
             for (let i = 0; i < result.length; i++){
@@ -24,7 +26,7 @@ router.get('/', (req, res) => {
                 items.push(item);
             }
             //console.log(items)
-            res.send(JSON.stringify({data:items}));
+            res.send(JSON.stringify({data:items,err:"",hasError:0}));
             //res.render('../views/home',{data:items});
         }
     });
--- client/src/App/pages/BikeTable.js
+++ client/src/App/pages/BikeTable.js
@@ -66,7 +66,7 @@ function BikeTable(){
 		fetchBikes();
 	}, []);
 
-	const [bikes, setBikes] = useState([]);
+	const [bikes, setBikes] = useState({});
 
 	const fetchBikes = async () => {
 		const data = await fetch(
@@ -74,19 +74,24 @@ function BikeTable(){
 		);
 
 		const bikes = await data.json();
-		setBikes(bikes.data);
+
+		setBikes(bikes);
+		console.log(bikes);
 	};
+
+	
 
 	return (
 		<div className="App">
 			<h1>Bike Table</h1>
-			{bikes.length ? (
+			{(bikes.hasOwnProperty("hasError") && !bikes.hasError) ? (
 				<div>
-					<Table data={bikes}/>
+					<Table data={bikes.data}/>
 				</div>
 			) : (
 				<div>
 					<h2>Error: no bikes were found</h2>
+					<p>{JSON.stringify(bikes.err)}</p>
 				</div>
 			)}
 		</div>
