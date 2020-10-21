eeeba3eca2b69bae77356b6d8e080f6894a17af4
sgilbo618@gmail.com <sgilbo618@gmail.com>
2020-10-21 10:13:31 -0400

    Add font awesome icons

--- client/src/App/components/MyNavbar.js
+++ client/src/App/components/MyNavbar.js
@@ -5,17 +5,22 @@ import { Link } from 'react-router-dom';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const MyNavbar = (props) => {
 	return (
 	  <Navbar scrolling dark expand="md" fixed="top">
-        <Navbar.Brand as={Link} to="./">Turbike</Navbar.Brand>
+        <Navbar.Brand as={Link} to="./">Turbike
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
           	<Nav.Link as={Link} to="#">List your bike</Nav.Link>
           	<Nav.Link as={Link} to="./login">Log in</Nav.Link>
-            <Nav.Link as={Link} to="./register">Sign up</Nav.Link>
+            <Nav.Link as={Link} to="./register">Sign up 
+              <FontAwesomeIcon className="user-icon" icon={faUser} />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
