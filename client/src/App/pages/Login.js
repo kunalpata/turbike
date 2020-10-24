c39a9dc423ca9c1156f134e1436eeb3fbbb377d1
Kunal Patadia <kunalpata@gmail.com>
2020-10-24 12:20:52 -0700

    Updated Login page still need to fix styling

--- client/src/App/pages/Login.js
+++ client/src/App/pages/Login.js
@@ -1,10 +1,17 @@
 // Login.js
 
 import React, { Component , useEffect, useState} from 'react';
-import '../App.css';
+import './Login.css';
 
 import MyNavbar from "../components/MyNavbar";
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import {Link} from "react-router-dom";
 
 function Login (){
 
@@ -30,12 +37,38 @@ function Login (){
 
 	return (
 		<div className="Login">
-			<div>
-				<h1>Login</h1>
-				<input type="text" name="username" placeholder="username" onChange={textChangeHandler}></input>
-				<input type="password" name="password" onChange={textChangeHandler}></input>
-				<button onClick={login}>Login</button>
-			</div>
+			<Container>
+				<Card style={{ width: '30rem', height: '20rem' }}>
+					<Row>
+						<Col>
+							<Card.Img variant="top" src={require("../images/turbike_logo.png")} style={{width: '17vh', height: '17vh'}} />
+						</Col>
+						<Col>
+							<Card.Title style={{ fontSize: '4vh' }}>Login</Card.Title>
+						</Col>
+					</Row>
+					<Card.Body>
+						<Card.Text>
+							Welcome Back!
+						</Card.Text>
+						<Form>
+							<Form.Control className="col-md" type="text" placeholder="Email" name="email" />
+						</Form>
+						<Form>
+							<Form.Control className="col-md" type="password" placeholder="Password" name="email" />
+						</Form>
+						<Link to={'./'}>
+							<Button variant="danger" >Login</Button>
+						</Link>
+					</Card.Body>
+				</Card>
+			</Container>
+			{/*<div>*/}
+			{/*	<h1>Login</h1>*/}
+			{/*	<input type="text" name="username" placeholder="username" onChange={textChangeHandler}></input>*/}
+			{/*	<input type="password" name="password" onChange={textChangeHandler}></input>*/}
+			{/*	<button onClick={login}>Login</button>*/}
+			{/*</div>*/}
 		</div>
 	);
 }
