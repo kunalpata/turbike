253ea40c84335f1c669ec59db17e07e7a70e7ee1
sgilbo618@gmail.com <sgilbo618@gmail.com>
2020-10-27 21:46:47 -0400

    basic layout for bike views

--- client/src/App/pages/BikeView.js
+++ client/src/App/pages/BikeView.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import './BikeView.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
 
 
 const BikeView = (props) => {
@@ -7,10 +13,47 @@ const BikeView = (props) => {
 	const bike = props.location.state.bike;
 
     return(
-    	<div>
-    		<h1>Bike View</h1>
-    		<div>{bike.price}</div>
-    	</div>
+    	<Container className="bike-view-body">
+    		<Row>
+    			<Col md={{span: 6, offset: 0}}>
+					<div id="bike-name" className="bike-header">Bike Name</div>
+					<div className="bike-header">
+						<img id="star-img" alt="star" src={require("../images/star_200.png")} height="15vh" width="15vh"/>
+						4.5   
+					</div>
+					<div id="category" className="bike-header">Road Bike</div>
+					<div className="float-right">
+						<img alt="clear heart" src={require("../images/clear_heart_200.png")} height="20vh" width="20vh"/>
+					</div>
+
+					<div>{bike.city}, {bike.state}</div>
+    				
+    				{/* TODO: get bike images and add them here with map */}
+					<Carousel>
+					  <Carousel.Item>
+					    <img
+					      className="d-block w-100"
+					      src={require("../images/road_bike.jpg")}
+					      alt="First slide"
+					    />
+					  </Carousel.Item>
+					  <Carousel.Item>
+					    <img
+					      className="d-block w-100"
+					      src={require("../images/road_bike.jpg")}
+					      alt="Second slide"
+					    />
+					  </Carousel.Item>
+					</Carousel>
+
+					<p>{bike.bike_details}</p>
+    			</Col>
+
+    			<Col md={{span: 6, offset: 0}}>
+    				
+    			</Col>
+    		</Row>
+    	</Container>
     );
 };
 
