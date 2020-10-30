d45b1592781dc6b39413cb33b8ff0b1980aef338
sgilbo618@gmail.com <sgilbo618@gmail.com>
2020-10-30 12:51:26 -0400

    add form ui

--- client/src/App/pages/BikeView.js
+++ client/src/App/pages/BikeView.js
@@ -5,6 +5,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 const BikeView = (props) => {
@@ -49,7 +52,50 @@ const BikeView = (props) => {
     			</Col>
 
     			<Col md={{span: 6, offset: 0}}>
-    				
+					<Card>
+					  <Card.Body>
+					    <Card.Title>${bike.price}/hour</Card.Title>
+					    <Card.Subtitle className="mb-2 text-muted">Est. Total: $</Card.Subtitle>
+					    <Form>
+					    	<Form.Row>
+								<Form.Group as={Col} controlId="sDate">
+								    <Form.Label>Start Date</Form.Label>
+								    <Form.Control type="text" />
+								</Form.Group>
+								<Form.Group as={Col} controlId="sTime">
+									<Form.Label>Start Time</Form.Label>
+								    <Form.Control type="text" />
+								</Form.Group>
+							</Form.Row>
+
+							<Form.Row>
+								<Form.Group as={Col} controlId="eDate">
+								    <Form.Label>End Date</Form.Label>
+								    <Form.Control type="text" />
+								</Form.Group>
+								<Form.Group as={Col} controlId="eTime">
+									<Form.Label>End Time</Form.Label>
+								    <Form.Control type="text" />
+								</Form.Group>
+							</Form.Row>
+
+							<Form.Row>
+								<Form.Group as={Col} controlId="sLoc">
+								    <Form.Label>Pick Up Location</Form.Label>
+								    <Form.Control type="text" />
+								</Form.Group>
+								<Form.Group as={Col} controlId="eLoc">
+									<Form.Label>Return Location</Form.Label>
+								    <Form.Control type="text" />
+								</Form.Group>
+							</Form.Row>
+							  
+							<Button className="reservation-button" block>
+							    Continue
+							</Button>
+					    </Form>
+					  </Card.Body>
+					</Card>
     			</Col>
     		</Row>
     	</Container>
