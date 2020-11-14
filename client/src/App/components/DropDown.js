bdb45b9cacc20346fa3fc073444dee53211e95f0
darwinyee <darwinmtyee@gmail.com>
2020-11-13 21:43:48 -0800

    Refactor out dropdown and feature components for reuse

---
+++ client/src/App/components/DropDown.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import '../bootstrap/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+
+
+
+const DropDown = (props) => {
+    console.log(props);
+
+    //props has label, name, sendSelected (function)
+
+    const [options, setOptions] = useState([]);
+
+    const fetchCategory = async ()=>{
+        await fetch('/api/get/categories')
+        .then((res) => {return res.json()})
+        .then((res) => {console.log(res); setOptions(res.data)})
+        .catch((err) => {console.log(err)});
+    }
+
+    const fetchState = ()=>{
+        const USstates = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA",
+                          "MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN",
+                          "TX","UT","VT","VA","WA","WV","WI","WY"];
+        setOptions(USstates.map((eachState, index) => {
+            return {id:index,name:eachState};
+        }));
+    }
+
+    const textChangeHandler = (e) => {
+        props.sendSelected(e.target.name,e.target.value);
+    }
+
+    const fillOptions = (name) => {
+        switch(name){
+            case "category":
+                fetchCategory();
+                break;
+            case "state":
+                fetchState();
+                break;
+            default:
+                break;
+        }
+    }
+
+    useEffect(()=>{
+        fillOptions(props.name); //retrieve options according to name
+    },[]);
+
+    return (
+        
+            <Form.Control as="select" name={props.name} defaultValue="Choose..." onChange={textChangeHandler}>
+                <option>Choose...</option>
+                {options.map((option) =>
+                     (<option key={option.id}>{option.name}</option>)   
+                )}
+            </Form.Control>
+       
+    )
+}
+
+export default DropDown;
